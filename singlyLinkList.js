3d0d185717a190a4138ef1cfa642924900f976f0
AbhinavDupare <abhinavdupare@gmail.com>
2020-07-27 18:51:25 +0530

    linklist add node updated to code

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -1,8 +1,37 @@
-// hi this abhinav using git
-console.log("hello world")
-//hi
-console.log("hello world from github")
-//hello
-console.log("hello from visual studio code")
-//changes 
-//another note
+class ListNode{
+    constructor(data){
+        this.data = data
+        this.next = null
+}
+class LinkList{
+    constructor(){
+        this.head = null
+        this._length = 0
+    }
+    addNode(data){
+        var newNode = new ListNode(data)
+        var currentNode 
+        // if list is empty
+        if(this.head = null){
+            this.head = newNode
+            this._length++
+            return newNode
+        }
+        else{
+            currentNode = this.head
+            while(currentNode.next){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+            this._length;
+            return newNode
+        }
+    }
+    insertNodeAt(data, index){
+        if(index > 0 && index > this._length){
+            return false
+            
+        }
+    }
+}
+
